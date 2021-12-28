5df5c46e859c41348695b57f153091730f13a121
Anton Zujev <azujev@vendavo.com>
2021-12-28 21:42:22 +0100

    refactoring/finishing map

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
+import mapboxgl from 'mapbox-gl'
 
-export const Map = () => {
-    const screenHeight = (window.screen.availHeight - 250) + 'px';
-
-    return <><iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A1f77aa9f31691ebf59ef088fab4003dd1273ff8092d7e2db9800794af505a3ef&amp;source=constructor" width="100%" height={screenHeight} frameBorder="0"></iframe></>
+export class Map extends Component {
+    
 }
+
+// export const Map = () => {
+//     const screenHeight = (window.screen.availHeight - 250) + 'px';
+
+//     return <><iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A1f77aa9f31691ebf59ef088fab4003dd1273ff8092d7e2db9800794af505a3ef&amp;source=constructor" width="100%" height={screenHeight} frameBorder="0"></iframe></>
+// }
