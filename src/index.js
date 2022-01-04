1d64598931d0d24f3893996056b923b0b817d15c
Anton Zujev <azujev@vendavo.com>
2022-01-04 10:22:28 +0100

    removed theme

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { theme } from 'loft-taxi-mui-theme'; // Импортируем саму тему
-import { MuiThemeProvider } from '@material-ui/core/styles';
 import { AppProvider } from './helpers/AppContext';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-      <MuiThemeProvider theme={theme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </MuiThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
