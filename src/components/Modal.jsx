85bca59b6ea69da245442ba5e9f4da40ba6b4f00
Anton Zujev <azujev@vendavo.com>
2022-01-09 23:13:30 +0100

    typo

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 export const Modal = (props) => {
   const cls =
-    (props.absolute ? 'modal-window-abs ' : 'modal-window-noabs') +
+    (props.absolute ? 'modal-window-abs ' : 'modal-window-noabs ') +
     props.height;
   return <div className={cls}>{props.children}</div>;
 };
