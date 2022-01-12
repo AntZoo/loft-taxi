bbf11e962a7152082bbbf64b3ce0025bbf63d61d
Anton Zujev <azujev@vendavo.com>
2022-01-12 14:07:51 +0100

    label now optional for input el + default value prop

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 export const InputBox = (props) => {
   return (
     <>
-      <label htmlFor={props.Id}>{props.LabelName}:</label>
+      {props.LabelName && <label htmlFor={props.Id}>{props.LabelName}:</label>}
       <input
         id={props.Id}
         name={props.Id}
         type={props.Type}
         placeholder={props.Placeholder}
+        defaultValue={props.Value}
       />
     </>
   );
